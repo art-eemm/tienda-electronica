2d85a3544c9fa14d2bc43149239968d48032cab5
agarciaa99 <aega2399@outlook.com>
2026-02-10 10:38:42 -0600

    Fix: Errores en el login solucionados

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,6 +1,8 @@
 "use client"; // se usa porque es necesario interactuar con el usuario
 
 import { useState } from "react";
+import { useRouter } from "next/navigation"; // redirigir
+import { supabase } from "@/lib/supabase"; // cliente
 import { Button } from "./ui/button";
 import {
   Card,
@@ -14,31 +16,74 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 
 export function LoginForm() {
-  // lógica de estado
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Iniciando sesión...");
+    setLoading(true);
+    setError(null);
+
+    try {
+      // llamada a supabase para iniciar sesión
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) {
+        setError(error.message);
+      } else {
+        router.push("/dashboard");
+        router.refresh();
+      }
+    } catch (err) {
+      setError(`Ocurrió un error inesperado ${err}`);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
-        <CardTitle className="text-2xl">Iniciar Sesión</CardTitle>
+        <CardTitle className="text-2xl">Iniciar sesión</CardTitle>
         <CardDescription>
-          Ingresa tu correo y contraseña para ingresar
+          Ingresa tus credenciales para ingresar
         </CardDescription>
       </CardHeader>
       <form onSubmit={handleSubmit}>
         <CardContent className="grid gap-4">
           <div className="grid gap-2">
             <Label htmlFor="email">Correo</Label>
-            <Input id="password" type="password" required />
+            <Input
+              id="email"
+              type="email"
+              placeholder="tu@gmail.com"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Contraseña</Label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="*****"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {error && (
+            <div className="text-red-500 text-sm font-medium">{error}</div>
+          )}
         </CardContent>
         <CardFooter>
-          <Button className="w-full" type="submit">
-            Entrar
+          <Button className="w-full mt-5" type="submit" disabled={loading}>
+            {loading ? "Cargando..." : "Iniciar sesión"}
           </Button>
         </CardFooter>
       </form>
